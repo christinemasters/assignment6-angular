1a76881c093e25b98eb7ff7f9fce1760d0bbeb3f
christinemasters <60408634+christinemasters@users.noreply.github.com>
2023-04-24 21:04:16 -0500

    1 file modified, 1 added

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import { enableProdMode } from '@angular/core';
+import { environment } from './environments/environment';
 import { AppModule } from './app/app.module'; // imports AppModule from src/app.module.ts
 import 'zone.js';
+
+if (environment.production) {
+  enableProdMode();
+}
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule) // 'boots' AppModule as our application
