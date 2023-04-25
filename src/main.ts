8179de59cdd2840b9a312222298d96b15399712d
christinemasters <60408634+christinemasters@users.noreply.github.com>
2023-04-24 20:47:56 -0500

    3 files modified

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module'; // imports AppModule from src/app.module.ts
+import 'zone.js';
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule) // 'boots' AppModule as our application
